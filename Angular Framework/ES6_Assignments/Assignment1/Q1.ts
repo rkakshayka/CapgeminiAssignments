72884a4b0fa816987b6e67d79217aafd73c5d000
AKshay KAushik <51936386+rkakshayka@users.noreply.github.com>
2022-05-03 18:05:05 +0530

    Delete Angular Framework directory

--- Angular Framework/ES6_Assignments/Assignment1/Q1.ts
+++
@@ -1,2 +0,0 @@
-const PI = 3.14;
-//PI = 6; //Error: Cannot assign to 'PI' because it is a constant.
--- Angular Framework/ES6_Assignments/Assignment1/Q6.ts
+++
@@ -1,6 +0,0 @@
-var laptop = "DELL", model = "Inspiron 15 3000 Series", deskno = 66, username='Akshay Kaushik';
-var ticket = `Hi Sysnet, this is ${username} - deskNo.: ${deskno} and my laptop: ${laptop}, model: ${model}, 
-hangs a lot. Is there any way to bypass this issue.`;
-
-console.log("-----Question 6-----");
-console.log(ticket);
